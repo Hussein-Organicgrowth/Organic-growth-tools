9240454db343ab515646ede7fc266821549e88e4
Hussein Ali <ha@organicgrowth.dk>
2023-08-01 12:50:31 +0200

    group keyword tool now allows unlimited keywords

--- public/js/opdel-keyword.js
+++ public/js/opdel-keyword.js
@@ -10,10 +10,116 @@ submit.addEventListener("click", async function (event) {
     return;
   }
 
-  generate(keywordsValue);
+  await generate(keywordsValue);
 });
 
-async function generate(keywords) {
+async function generate(keywordsString) {
+  const CHUNK_SIZE = 200;
+  let keywords = keywordsString.split("\n");
+
+  // Split the keywords into chunks of 200
+  const chunks = [];
+  for (let i = 0; i < keywords.length; i += CHUNK_SIZE) {
+    chunks.push(keywords.slice(i, i + CHUNK_SIZE));
+  }
+
+  // Loop over chunks
+  for (let chunk of chunks) {
+    // Process each chunk
+
+    await processChunk(chunk);
+  }
+}
+
+async function processChunk(chunk) {
+  // Create a new AbortController instance
+  if (textArea.value != "") {
+    textArea.value += "\n";
+  }
+  controller = new AbortController();
+  const signal = controller.signal;
+
+  let prompt = "søgeord: {søgeord}";
+  prompt = prompt.replace("{søgeord}", chunk.join(", "));
+
+  // rest of your prompt setup...
+  let titlePrompt =
+    "Imagine yourself as an expert in keyword analysis and SEO, particularly skilled in categorizing keywords. Your task will be to arrange a set of Danish keywords into relevant categories. " +
+    "You will be asked to provide these specific keywords after this prompt. " +
+    "As you perform this task, it's important to demonstrate a thorough understanding of the Danish language and the nuances of keyword grouping. " +
+    "The categories you create should reflect the meaning and intent behind the keywords, helping to improve the structure of a Danish language SEO campaign. " +
+    "Here are some guidelines to follow: " +
+    "Each keyword should be placed into the category that best matches its meaning and potential user intent. " +
+    "The categories should be named in Danish, reflecting the overall theme of the keywords within. " +
+    "Prioritize logical organization over the quantity of categories. It's better to have a few well-structured categories than numerous disorganized ones. " +
+    "When in doubt, consider the possible search queries a user might input when looking for the kind of content or services the keywords are targeting. " +
+    "After this prompt, you will be asked to provide the specific Danish keywords. With these keywords in hand, start organizing them into well-defined, relevant categories. This activity will not only enhance the SEO strategy but will also contribute to a more targeted and effective digital marketing campaign. " +
+    "ALWAYS FOLLOW THIS FORMAT. Category:keywords. The keywords will be split by commas making it an array ALWAYS FOLLOW THIS and It will look like this Category:name of the category under that søgeord:. ";
+  try {
+    const response = await fetch("/process-text", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        titlePrompt: titlePrompt,
+        prompt: prompt,
+      }),
+    });
+
+    // rest of your processing code...
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+
+    let buffer = "";
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+
+      buffer += decoder.decode(value);
+
+      while (buffer.includes("\n")) {
+        const newlineIndex = buffer.indexOf("\n");
+        const line = buffer.slice(0, newlineIndex);
+        buffer = buffer.slice(newlineIndex + 1);
+
+        if (line.trim()) {
+          const jsonLine = line.replace(/^data: /, "").trim();
+          console.log("JSONLINE: " + jsonLine);
+          if (!jsonLine.includes("[DONE]")) {
+            try {
+              const parsedData = JSON.parse(jsonLine);
+              const content = parsedData.choices[0].delta.content;
+
+              if (typeof content !== "undefined") {
+                textArea.value += content;
+              }
+            } catch (error) {
+              console.error("Error parsing JSON:", error);
+            }
+          }
+        }
+      }
+    }
+  } catch (error) {
+    // Handle fetch request errors
+    if (signal.aborted) {
+      textArea.innerText = "Request aborted.";
+    } else {
+      console.error("Error:", error);
+      textArea.innerText = "Error occurred while generating.";
+    }
+  } finally {
+    // Enable the generate button and disable the stop button
+    controller = null; // Reset the AbortController instance
+  }
+}
+
+async function processChunkeee(keywords) {
   // Create a new AbortController instance
   textArea.value = "";
   controller = new AbortController();
@@ -75,8 +181,7 @@ async function generate(keywords) {
           const jsonLine = line.replace(/^data: /, "").trim();
           console.log("JSONLINE: " + jsonLine);
           if (jsonLine.includes("[DONE]")) {
-            alert("Du kan nu downloade din csv file");
-            return;
+            continue;
           }
           try {
             const parsedData = JSON.parse(jsonLine);
